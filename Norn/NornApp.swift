5778d42d525f768c28a184b2569a98d63dbe5a98
Pavel Neprin <41507524+neprin@users.noreply.github.com>
2022-10-06 22:07:27 +0300

    Initial Commit

---
+++ Norn/NornApp.swift
@@ -0,0 +1,17 @@
+//
+//  NornApp.swift
+//  Norn
+//
+//  Created by Pavel Neprin on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct NornApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
